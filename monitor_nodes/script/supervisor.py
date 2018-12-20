31d54910e67de6e3cada96831313ff8e4720dad3
tina <van@uni-bremen.de>
2018-12-20 15:10:10 +0100

    changed loggger_level from int to byte, service get_logger_level still not working

--- monitor_nodes/script/supervisor.py
+++ monitor_nodes/script/supervisor.py
@@ -16,11 +16,9 @@ class SupervisorNode(object):
         initialisation of an object from SupervisorNode.
         subscribed to rosout (rosout_agg)
         """
-        #msgsInfo = []
-        self.sub = rospy.Subscriber("rosout_agg", Log, self.updateLoggerList)
+        self.sub = rospy.Subscriber("rosout_agg", Log, self.update_logger_list)
         self.srv = rospy.Service("~get_logger_level", GetLogger, self.get_logger_level)
         self.loglist= []
-        #self.loglist = np.array(msgsInfo)
         self.logger = logging.getLogger("rosout")
 
 
@@ -30,16 +28,16 @@ class SupervisorNode(object):
         :param msg:
         :return: Nodes
         """
+
         for x in self.loglist:
-            print(x)
-        #return self.loglist
+           print(x)
         #logger.setLevel(logging.INFO)
         #logger.setLevel(level)
 
-    def updateLoggerList(self, msg):
+    def update_logger_list(self, msg):
         """
         Callback to ROS topic that processes new message.
-        and saves the origin node of the msg in loglist.
+        and saves the origin node of the msg and the logger_level in loglist.
         :param msg: The newest ROS message to process.
         :type msg: rosout
         :return: Nothing.
@@ -47,14 +45,15 @@ class SupervisorNode(object):
         """
         msgsInfo = [msg.name, msg.level]
 
-        #for x in np.nditer(self.loglist):
+        # if not self.loglist:
+        #     self.loglist.append(msgsInfo)
+
         for x in self.loglist:
-            if msgsInfo[0] == x[0]:
+            if x[0] == msgsInfo[0]:
                 if msgsInfo[1] > x[1]:
-                    self.loglist[x, msgsInfo]
-                    print("works!")
+                    self.loglist.insert(self.loglist.index(x), msgsInfo)
             elif x == self.loglist[-1]:
-                self.loglist = np.concatenate((self.loglist, msgsInfo), axis = 0)
+                self.loglist = self.loglist.append(msgsInfo)
 
         # ______
         #
@@ -66,17 +65,6 @@ class SupervisorNode(object):
         #             self.loglist.insert(index_of_present_msg_in_loglist, msgsInfo)
         # else:
         #     self.loglist = np.append(self.loglist, msgsInfo)
-
-    def handleStatistics(self, req):
-        """
-        service that returns the processes information of SpeakingNumber.
-        :param req: New service request.
-        :type req: GetCountRequest
-        :return: number of messengers received, firstmsg and lastmsg received
-        :rtype: dict
-        """
-       # return {'num_messages':self.msg_count,'first_message': self.first_msg, 'last_message': self.last_msg}
-
 
 if __name__ == '__main__':
     rospy.init_node('supervisor_node', anonymous=True)
