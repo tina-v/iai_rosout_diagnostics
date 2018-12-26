222718571d53ccd540877b55b85ad435ed7155b8
tina <van@uni-bremen.de>
2018-12-26 19:20:46 +0100

    first look at diagnostic, added service 'set_logger_level_of' in supercisor.py

--- monitor_nodes/script/supervisor.py
+++ monitor_nodes/script/supervisor.py
@@ -8,31 +8,31 @@ from drive_msgs.srv import GetStatistics, GetStatisticsRequest
 from rosgraph_msgs.msg import Log
 from rospy.service import logger
 from drive_msgs.srv import GetLogger
-from sets import Set
+from diagnostic_msgs.msg import DiagnosticArray
 
 class SupervisorNode(object):
     def __init__(self):
         """
         initialisation of an object from SupervisorNode.
         subscribed to rosout (rosout_agg)
+        publishes in diagnostic topic as diagnostic_msgs/DiagnosticArray
+        when it receives a msg from rosout.
         """
         self.sub = rospy.Subscriber("rosout_agg", Log, self.update_logger_list)
-        self.srv = rospy.Service("~get_logger_level", GetLogger, self.get_logger_level)
-        self.loglist= []
+        self.srv = rospy.Service("~set_logger_level_of", GetLogger, self.set_logger_level_of_other_node)
+        self.pub = rospy.Publisher("~diagnostic", DiagnosticArray,  queue_size=10)
+        self.loglist = []
         self.logger = logging.getLogger("rosout")
 
 
-    def get_logger_level(self, srv):
+    def set_logger_level_of_other_node(self,srv):
         """
-        returns List of existing Loggers (and their log_level)
-        :param msg:
-        :return: Nodes
+        Setting a new level for a Node
+        :param srv: GetLogger
+        :return: None
         """
+        logging.getLogger(srv.name).setLevel(srv.logger_level)
 
-        for x in self.loglist:
-           print(x)
-        #logger.setLevel(logging.INFO)
-        #logger.setLevel(level)
 
     def update_logger_list(self, msg):
         """
@@ -40,20 +40,24 @@ class SupervisorNode(object):
         and saves the origin node of the msg and the logger_level in loglist.
         :param msg: The newest ROS message to process.
         :type msg: rosout
-        :return: Nothing.
+        :return: Nothing
         :rtype: None
         """
+
         msgsInfo = [msg.name, msg.level]
 
-        # if not self.loglist:
-        #     self.loglist.append(msgsInfo)
+        self.pub.publish(self, msgsInfo)
 
-        for x in self.loglist:
-            if x[0] == msgsInfo[0]:
-                if msgsInfo[1] > x[1]:
-                    self.loglist.insert(self.loglist.index(x), msgsInfo)
-            elif x == self.loglist[-1]:
-                self.loglist = self.loglist.append(msgsInfo)
+        #TODO die abfrage ist das problem-
+       # if not self.loglist:
+        #    self.loglist.append(msgsInfo)
+        #
+        # for x in self.loglist:
+        #     if x[0] == msgsInfo[0]:
+        #         if msgsInfo[1] > x[1]:
+        #             self.loglist.insert(self.loglist.index(x), msgsInfo)
+        #     elif x == self.loglist[-1] or self.loglist[0]:
+        #         self.loglist.append(msgsInfo)
 
         # ______
         #
